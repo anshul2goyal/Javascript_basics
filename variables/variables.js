26547967a65da96c615b66230f1c8ea3cc3ef9c6
Anshul Goyal <anshulgoyal@Anshuls-MacBook-Pro.local>
2018-08-21 21:09:38 +0100

    Updated the var and let difference

--- variables/variables.js
+++ variables/variables.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+/* 
+ * Example to demonstrate the difference between var and let 
+ *  
+ *	
+*/
+//When scope is global for var and let
+var a = "test"
+let b = "I am let"
+
+console.log(window.a) //output -> test
+console.log(window.b) //output -> undefined
+
+// So the global variables defined with let will not be added as properties on global windows object
+
+
